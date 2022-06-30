55cc3f42fccc26a11c64ea904efdfccbdcc458b7
rasdani <daniel.auras94@gmx.de>
2022-06-30 16:04:40 +0200

    refactoring frontend

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,70 +1,19 @@
 import { useState, useEffect } from 'react'
+import Person from './components/Person'
+import AllPersons from './components/AllPersons'
+import Search from './components/Search'
+import Notification from './components/Notification'
 import personsService from './services/persons'
 import './index.css'
 
-//const Form = () => {
-  //return (
-    //<>
-      //<form onSubmit={addPerson}>
-        //<div>
-          //name: <input value={newName} onChange={handleNameChange} />
-        //</div>
-        //<div>
-          //number: <input value={newNumber} onChange={handleNumberChange} />
-        //</div>
-        //<div>
-          //<button type="submit">add</button>
-        //</div>
-      //</form>
-    //</>
-  //)
-//}
-
-const Search = (props) => {
-  const persons = [...props.persons]
-  const matches = persons
-                    .filter(person => person.name
-                                              .toLowerCase()
-                                              .includes(props
-                                              .searchName
-                                              .toLowerCase()))
-  const handleDelete = props.handleDelete
-  return (
-    <AllPersons persons={matches} handleDelete={handleDelete} />
-  )
-}
-
-const Person = ({ person , handleDelete}) => {
-  return (
-    <p key={person.id}>
-      {person.name} {person.number} 
-      <button key={person.id} onClick={event => handleDelete(event, person)}>
-        delete
-      </button>
-    </p>
-  )
-}
-
-const AllPersons = ({ persons , handleDelete}) => {
-  return (
-    persons.map(person => <Person key={person.id} person={person} handleDelete={handleDelete} />)
-  )
-}
-
-const Notification = ({ message , type}) => {
-  if (message === null) {
-    return null
-  }
-
-  return (
-    <div className={type}>
-      {message}
-    </div>
-  )
-}
 
 const App = () => {
   const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [searchName, setSearchName] = useState('')
+  const [notificationMessage, setNotificationMessage] = useState(null)
+  const [notificationType, setNotificationType] = useState('succesful')
 
   useEffect(() => {
     personsService
@@ -73,12 +22,6 @@ const App = () => {
         setPersons(recievedPersons)
       })
   }, [])
-
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [searchName, setSearchName] = useState('')
-  const [notificationMessage, setNotificationMessage] = useState(null)
-  const [notificationType, setNotificationType] = useState('succesful')
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
