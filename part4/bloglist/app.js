5e94e3cfe7e8a079cf4f7137a99e73397fd834a9
rasdani <daniel.auras94@gmx.de>
2022-07-22 21:08:43 +0200

    exercise 4.2 done

---
+++ part4/bloglist/app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const logger = require('./utils/logger')
+const config = require('./utils/config')
+const Blog = require('./models/blog')
+const blogsRouter = require('./controllers/blogs')
+
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI)
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/blogs', blogsRouter)
+
+
+module.exports = app
