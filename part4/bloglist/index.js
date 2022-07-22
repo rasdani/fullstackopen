d97e5ad6c0b47fdafc776b9babf0e77fc6876635
rasdani <daniel.auras94@gmx.de>
2022-07-22 20:52:01 +0200

    refactored router

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 const config = require('./utils/config')
 const Blog = require('./models/blog')
+const blogsRouter = require('./controllers/blogs')
 
 //const blogSchema = new mongoose.Schema({
   //title: String,
@@ -22,26 +23,27 @@ mongoose.connect(mongoUrl)
 
 app.use(cors())
 app.use(express.json())
+app.use('/api/blogs', blogsRouter)
 
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+//app.get('/api/blogs', (request, response) => {
+  //Blog
+    //.find({})
+    //.then(blogs => {
+      //response.json(blogs)
+    //})
+//})
 
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
+//app.post('/api/blogs', (request, response) => {
+  //const blog = new Blog(request.body)
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+  //blog
+    //.save()
+    //.then(result => {
+      //response.status(201).json(result)
+    //})
+//})
 
 
 const PORT = config.PORT
