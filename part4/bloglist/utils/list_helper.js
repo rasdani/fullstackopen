ea8524cc8ee148315dd4d7017db9a77512715bde
rasdani <daniel.auras94@gmx.de>
2022-07-23 10:02:04 +0200

    exercise 4.3 done

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,18 @@ const dummy = (blogs) => {
   return 1
 }
 
+
+const totalLikes = (blogs) => {
+  const summer = (sum, item) => {
+    return sum + item
+  }
+
+  const likes = blogs.map(blogs => blogs.likes)
+
+  return likes.reduce(summer, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
 }
