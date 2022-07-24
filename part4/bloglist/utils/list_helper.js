d1f652b076b97743599278a53dff3b6264aa3833
rasdani <daniel.auras94@gmx.de>
2022-07-24 12:56:53 +0200

    exercise 4.6 done

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash')
+
 const dummy = (blogs) => {
   return 1
 }
@@ -13,7 +15,21 @@ const totalLikes = (blogs) => {
   return likes.reduce(summer, 0)
 }
 
+const mostBlogs = (blogs) => {
+  const numberOfBlogs = _.countBy(blogs.map(blog => blog.author))
+  const maxi = _.max(Object.values(numberOfBlogs))
+  const idxOfMax = Object.values(numberOfBlogs).indexOf(maxi)
+  const maxBlogger = Object.keys(numberOfBlogs)[idxOfMax]
+
+  return {
+    'author': maxBlogger,
+    'blogs': maxi
+  }
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  mostBlogs,
 }
+
