d7d40c5dd8ddf04d9a54227fba09640555bdd7b3
rasdani <daniel.auras94@gmx.de>
2022-08-25 20:00:52 +0200

    exercise 4.8 done

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,23 +1,34 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
+//blogsRouter.get('/', (request, response) => {
+  //Blog
+    //.find({})
+    //.then(blogs => {
+      //response.json(blogs)
+    //})
+//})
+
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
+//blogsRouter.post('/', (request, response) => {
+  //const blog = new Blog(request.body)
 
-blogsRouter.post('/', (request, response) => {
+  //blog
+    //.save()
+    //.then(result => {
+      //response.status(201).json(result)
+    //})
+//})
+
+blogsRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
+  const savedBlog = await blog.save()
+  response.status(201).json(savedBlog)
 })
 
 module.exports = blogsRouter
