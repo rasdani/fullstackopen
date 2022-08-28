d1bdc027557986dea7d23b2567458213ce09a32f
rasdani <daniel.auras94@gmx.de>
2022-08-28 19:36:06 +0200

    refactored tests and added helper functions for them

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -1,159 +1,125 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app.js')
+const helper = require('./test_helper.js')
 
 const api = supertest(app)
 
 const Blog = require('../models/blog.js')
 
-const initialBlogs = [
-    {
-      id: "5a422a851b54a676234d17f7",
-      title: "React patterns",
-      author: "Michael Chan",
-      url: "https://reactpatterns.com/",
-      likes: 7,
-      __v: 0
-    },
-    {
-      id: "5a422b3a1b54a676234d17f9",
-      title: "Canonical string reduction",
-      author: "Edsger W. Dijkstra",
-      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-      likes: 12,
-      __v: 0
-    },
-    {
-      id: "5a422b891b54a676234d17fa",
-      title: "First class tests",
-      author: "Robert C. Martin",
-      url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
-      likes: 10,
-      __v: 0
-    },
-    {
-      id: "5a422ba71b54a676234d17fb",
-      title: "TDD harms architecture",
-      author: "Robert C. Martin",
-      url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
-      likes: 0,
-      __v: 0
-    },
-    {
-      id: "5a422bc61b54a676234d17fc",
-      title: "Type wars",
-      author: "Robert C. Martin",
-      url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
-      likes: 2,
-      __v: 0
-    }
-]
 
 beforeEach(async () => {
   await Blog.deleteMany({})
 
-  //for (let blog of initialBlogs) {
+  //for (let blog of helper.initialBlogs) {
     //let blogObject = new Blog(blog)
     //await blogObject.save()
   //}
-  await Blog.insertMany(initialBlogs)
+  await Blog.insertMany(helper.initialBlogs)
 })
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+describe('tests on some intitial blogs', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('there are five blogs', async () => {
+    const response = await api.get('/api/blogs')
+
+    expect(response.body).toHaveLength(5)
+  })
+
+  test('unique identifier property is named properly', async () => {
+    const response = await api.get('/api/blogs')
+    
+    response.body.forEach(blog => expect(blog.id).toBeDefined())
+  })
 })
 
-test('there are five blogs', async () => {
-  const response = await api.get('/api/blogs')
+describe('tests on adding a new blog', () => {
+  test('a valid blog can be added', async () => {
+    const newBlog = 
+      {
+          id: '5a422aa71b54a676234d17f8',
+          title: 'Go To Statement Considered Harmful',
+          author: 'Edsger W. Dijkstra',
+          url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+          likes: 5,
+          __v: 0
+        }
+    
 
-  expect(response.body).toHaveLength(5)
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/blogs')
+
+    const titles = response.body.map(r => r.title)
+
+    expect(response.body).toHaveLength(helper.initialBlogs.length + 1)
+    expect(titles).toContain(
+          'Go To Statement Considered Harmful'
+    )
+  })
+
+  test('if likes property is missing default to 0', async () => {
+    const newBlog = 
+      {
+          id: '5a422aa71b54a676234d17f8',
+          title: 'Go To Statement Considered Harmful',
+          author: 'Edsger W. Dijkstra',
+          url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+          __v: 0
+        }
+    
+
+    const post_response = await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+    const id = post_response.body.id
+
+    const get_response = await api.get('/api/blogs')
+
+    const [ addedBlog ] = get_response.body.filter(r => r.id === id)
+
+    //expect(addedBlog.title).toBe('Go To Statement Considered Harmful')
+    expect(addedBlog.likes).toBe(0)
+  })
+
+  test('if title or url is missing respond with 400 BAD REQUEST', async () => {
+    const newBlog = 
+      {
+          id: '5a422aa71b54a676234d17f8',
+          author: 'Edsger W. Dijkstra',
+          likes: 5,
+          __v: 0
+        }
+    
+    
+
+    const post_response = await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+
+    const get_response = await api.get('/api/blogs')
+
+    expect(get_response.body).toHaveLength(helper.initialBlogs.length)
+
+  }, 15000)
 })
 
-test('unique identifier property is named properly', async () => {
-  const response = await api.get('/api/blogs')
-  
-  response.body.forEach(blog => expect(blog.id).toBeDefined())
-})
-
-test('a valid blog can be added', async () => {
-  const newBlog = 
-    {
-        id: '5a422aa71b54a676234d17f8',
-        title: 'Go To Statement Considered Harmful',
-        author: 'Edsger W. Dijkstra',
-        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-        likes: 5,
-        __v: 0
-      }
-  
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
-
-  const response = await api.get('/api/blogs')
-
-  const titles = response.body.map(r => r.title)
-
-  expect(response.body).toHaveLength(initialBlogs.length + 1)
-  expect(titles).toContain(
-        'Go To Statement Considered Harmful'
-  )
-})
-
-test('if likes property is missing default to 0', async () => {
-  const newBlog = 
-    {
-        id: '5a422aa71b54a676234d17f8',
-        title: 'Go To Statement Considered Harmful',
-        author: 'Edsger W. Dijkstra',
-        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-        __v: 0
-      }
-  
-
-  const post_response = await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
-  const id = post_response.body.id
-
-  const get_response = await api.get('/api/blogs')
-
-  const [ addedBlog ] = get_response.body.filter(r => r.id === id)
-
-  //expect(addedBlog.title).toBe('Go To Statement Considered Harmful')
-  expect(addedBlog.likes).toBe(0)
-})
-
-test('if title or url is missing respond with 400 BAD REQUEST', async () => {
-  const newBlog = 
-    {
-        id: '5a422aa71b54a676234d17f8',
-        author: 'Edsger W. Dijkstra',
-        likes: 5,
-        __v: 0
-      }
-  
-  
-
-  const post_response = await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
-
-  const get_response = await api.get('/api/blogs')
-
-  expect(get_response.body).toHaveLength(initialBlogs.length)
-
-}, 15000)
-
+//desrscribe('deletion of a blog', () => {
+  //test('succeds with status code 204 if id is valid', async () => {
+    
 afterAll(() => {
   mongoose.connection.close()
 })
