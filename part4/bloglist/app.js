f555e82705aa555db06850a6effe9685be85bf17
rasdani <daniel.auras94@gmx.de>
2022-10-09 00:33:38 +0200

    exercise 4.15 done

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -7,6 +7,7 @@ const logger = require('./utils/logger')
 const config = require('./utils/config')
 const Blog = require('./models/blog')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 
 
 logger.info('connecting to', config.MONGODB_URI)
@@ -16,6 +17,7 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 app.use(logger.requestLogger)
 
 
